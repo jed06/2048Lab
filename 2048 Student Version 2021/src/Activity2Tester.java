36589881ab194cfa9ef6446ab6302204da23cc47
1904636 <1904636@01-19813.pusd.dom>
2022-01-24 14:53:07 -0800

    4

--- 2048 Student Version 2021/src/Activity2Tester.java
+++ 2048 Student Version 2021/src/Activity2Tester.java
@@ -3,11 +3,19 @@ public class Activity2Tester {
 
 	public static void main(String[] args) {
 		//Create a Board object
+		Board b = new Board();
 		
+		b.populateOne();
+		b.populateOne();
+		b.populateOne();
+		b.populateOne();
+		b.populateOne();
+		b.populateOne();
+
 		//call the populateOne method and print out the board each time
 		//does it fill a random location? How about if the board is almost
 		//full? Does it still work?
-		
+		System.out.println(b);
 		
 	}
 
